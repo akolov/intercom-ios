e3e76538be21b205e327c885f6c4f2675f7689c5
Alexander Kolov <me@alexkolov.com>
2020-12-18 16:53:11 +0900

    Add SPM integration

---
+++ Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+  name: "Intercom",
+  platforms: [
+    .iOS(.v10)
+  ],
+  products: [
+    .library(
+      name: "Intercom",
+      targets: ["Intercom"]),
+  ],
+  targets: [
+    .binaryTarget(
+      name: "Intercom",
+      path: "Intercom.xcframework"
+    )
+  ]
+)
